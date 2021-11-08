03704e20d34eb47a4447d915f0f36575152761aa
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2021-11-08 10:26:12 +0300

    Create Main.java

---
+++ 08.11.2021/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 0.2;
+        double b = 0.1;
+        System.out.println(a+b); // 11
+        System.out.println(a-b); // 5
+        System.out.println(a*b); // 24
+        System.out.println(a/b); // ?
+        System.out.println(a%b); // 2
+        System.out.println(a>b); // true
+        System.out.println(a==b); // false
+        //String name = "Ivan";
+        //System.out.println("Hello "+name);
+    }
+}
